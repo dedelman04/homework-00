4095feb76ffb495e481cec165b4a968b3c7d19e9
David Edelman <dedelman@cameronlng.com>
2018-08-16 16:29:45 -0500

    initial load

---
+++ Environment.R
@@ -0,0 +1,19 @@
+setwd("H:/DEdelman/")
+
+library(data.table)
+library(RevoScaleR)
+library(dplyr)
+library(stringr)
+library(lubridate)
+library(rgeos) # spatial package
+library(sp) # spatial package
+library(maptools) # spatial package
+library(ggmap)
+library(ggplot2)
+library(gridExtra) # for putting plots side by side
+library(ggrepel) # avoid text overlap in plots
+library(tidyr)
+library(seriation) # package for reordering a distance matrix
+library(gtools)
+library(tidyverse)
+library(dslabs)
